6ae6dea10f0991707055179ea52b9bc0b2588d59
Yuki Ito <acomagu@gmail.com>
2017-05-16 22:01:35 +0900

    [Add] initial commit

---
+++ config.rb
@@ -0,0 +1,17 @@
+remap 'Alt-b', to: 'Left'
+remap 'Alt-f', to: 'Right'
+remap 'Alt-p', to: 'Up'
+remap 'Alt-n', to: 'Down'
+
+remap 'Alt-a', to: 'Home'
+remap 'Alt-e', to: 'End'
+
+remap 'Alt-u', to: 'Page_Up'
+remap 'Alt-d', to: 'Page_Down'
+
+remap 'C-Alt-u', to: 'C-Page_Up'
+remap 'C-Alt-d', to: 'C-Page_Down'
+
+window class_only: 'google-chrome' do
+  remap 'C-h', to: 'BackSpace'
+end
